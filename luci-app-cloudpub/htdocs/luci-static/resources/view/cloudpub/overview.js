5edce61e7b6536e18ce19ed5b1047785e5ecbb74
BrainDeLook <148875214+BrainDeLook@users.noreply.github.com>
2026-09-12 19:15:15 +0300

    fix(luci): make service restart button work

--- luci-app-cloudpub/htdocs/luci-static/resources/view/cloudpub/overview.js
+++ luci-app-cloudpub/htdocs/luci-static/resources/view/cloudpub/overview.js
@@ -13,6 +13,13 @@ var callServiceList = rpc.declare({
 	expect: { '': {} }
 });
 
+var callServiceAction = rpc.declare({
+	object: 'service',
+	method: 'action',
+	params: [ 'name', 'action' ],
+	expect: { '': {} }
+});
+
 function getServiceStatus() {
 	return L.resolveDefault(callServiceList('cloudpub'), {}).then(function(res) {
 		var running = false;
@@ -176,7 +183,7 @@ return view.extend({
 			_('Apply the token and re-register all publications.'));
 		o.inputstyle = 'apply';
 		o.onclick = function() {
-			return fs.exec('/etc/init.d/cloudpub', [ 'restart' ]).then(function() {
+			return callServiceAction('cloudpub', 'restart').then(function() {
 				ui.addNotification(null, E('p', _('CloudPub service restarted.')), 'info');
 			}).catch(function(e) {
 				ui.addNotification(null, E('p', e.message), 'error');
