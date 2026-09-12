b7a84534a01644e6a3429849aecfcdd8bc1354dd
BrainDeLook <148875214+BrainDeLook@users.noreply.github.com>
2026-09-12 19:32:09 +0300

    fix: restore stable publication sync and init restart

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -eu
 REPO="${REPO:-BrainDeLook/CloudPub-OpenWRT}"
 VERSION="${VERSION:-3.5.1056}"
 PKG_RELEASE="${PKG_RELEASE:-1}"
-LUCI_VERSION="${LUCI_VERSION:-1.5.3}"
-RELEASE_TAG="${RELEASE_TAG:-v3.5.1056-luci.7}"
+LUCI_VERSION="${LUCI_VERSION:-1.5.4}"
+RELEASE_TAG="${RELEASE_TAG:-v3.5.1056-luci.8}"
 BASE_URL="${BASE_URL:-https://github.com/$REPO/releases/download/$RELEASE_TAG}"
 TMP="/tmp/cloudpub-install.$$"
 trap 'rm -rf "$TMP"' EXIT INT TERM
--- luci-app-cloudpub/htdocs/luci-static/resources/view/cloudpub/overview.js
+++ luci-app-cloudpub/htdocs/luci-static/resources/view/cloudpub/overview.js
@@ -13,13 +13,6 @@ var callServiceList = rpc.declare({
 	expect: { '': {} }
 });
 
-var callServiceAction = rpc.declare({
-	object: 'service',
-	method: 'action',
-	params: [ 'name', 'action' ],
-	expect: { '': {} }
-});
-
 function getServiceStatus() {
 	return L.resolveDefault(callServiceList('cloudpub'), {}).then(function(res) {
 		var running = false;
@@ -183,7 +176,7 @@ return view.extend({
 			_('Apply the token and re-register all publications.'));
 		o.inputstyle = 'apply';
 		o.onclick = function() {
-			return callServiceAction('cloudpub', 'restart').then(function() {
+			return fs.exec('/etc/init.d/cloudpub', [ 'restart' ]).then(function() {
 				ui.addNotification(null, E('p', _('CloudPub service restarted.')), 'info');
 			}).catch(function(e) {
 				ui.addNotification(null, E('p', e.message), 'error');
